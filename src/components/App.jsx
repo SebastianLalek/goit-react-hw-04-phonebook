121d0ea446c37ed55f9521efec3dd36f2005b60b
Sebastian Lalek <chemiczq@gmail.com>
2023-11-18 21:28:52 +0100

    delete unnecessary  comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,7 @@ function Phonebook() {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
+  // add contact
   const addNewContact = e => {
     e.preventDefault();
     const newName = e.target.name.value;
@@ -38,6 +39,7 @@ function Phonebook() {
     setContacts(contacts => [...contacts, newContact]);
   };
 
+  // delete contact
   const deleteContact = e => {
     const contactId = e.target.id;
 
@@ -48,10 +50,12 @@ function Phonebook() {
     setContacts([...updatedContacts]);
   };
 
+  // prevent submit
   const preventSubmit = e => {
     e.preventDefault();
   };
 
+  // filter
   const handleChange = e => {
     setFilter(e.target.value);
   };
@@ -62,6 +66,7 @@ function Phonebook() {
     );
   };
 
+  // local storage
   useEffect(() => {
     const savedContacts = localStorage.getItem('contacts');
     const parsedContats = JSON.parse(savedContacts);
@@ -92,112 +97,6 @@ function Phonebook() {
     </div>
   );
 }
-
-// class Phonebook extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-//   handleChange = e => {
-//     e.preventDefault();
-//     const { name, value } = e.target;
-//     this.setState({ [name]: value });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-
-//     const name = this.state.name;
-//     const number = this.state.number;
-
-//     const newContact = {
-//       name: name,
-//       number: number,
-//       id: nanoid(),
-//     };
-
-//     if (
-//       this.state.contacts.find(
-//         contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-//       )
-//     ) {
-//       return Notiflix.Report.failure(
-//         'Error',
-//         `${newContact.name} is already in contacts`,
-//         'OK'
-//       );
-//     }
-
-//     this.setState({
-//       contacts: [...this.state.contacts, newContact],
-//     });
-//   };
-
-//   preventSubmit = e => {
-//     e.preventDefault();
-//   };
-
-//   filter = () => {
-//     return this.state.contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
-//     );
-//   };
-
-//   deleteContact = e => {
-//     const contactId = e.target.id;
-
-//     const updatedContacts = this.state.contacts.filter(
-//       contact => contact.id !== contactId
-//     );
-//     this.setState({
-//       contacts: [...updatedContacts],
-//     });
-//   };
-
-//   componentDidMount() {
-//     const savedContacts = localStorage.getItem('contacts');
-//     const parsedContats = JSON.parse(savedContacts);
-
-//     if (parsedContats === null) {
-//       return;
-//     }
-
-//     if (parsedContats.length > 0) {
-//       this.setState({ contacts: parsedContats });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const newState = this.state;
-//     if (prevState.contacts.length !== newState.contacts.length) {
-//       localStorage.setItem('contacts', JSON.stringify(newState.contacts));
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Section title="Phonebook">
-//           <Form onChange={this.handleChange} onSubmit={this.handleSubmit} />
-//         </Section>
-//         <Section title="Contacts">
-//           <ContactList contacts={this.filter()} onClick={this.deleteContact}>
-//             <Filter
-//               onChange={this.handleChange}
-//               onSubmit={this.preventSubmit}
-//             />
-//           </ContactList>
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
 
 export const App = () => {
   return (
